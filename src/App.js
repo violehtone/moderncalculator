0f6d630283e5bb54af78cdc89ddba658cd6cf935
Ville Lehtonen <ville.lehtonen@siili.com>
2018-05-30 15:34:43 +0300

    finalizing css

--- src/App.js
+++ src/App.js
@@ -33,8 +33,10 @@ class App extends Component {
           <h1 className="App-title">Calculator</h1>
         </header>
         <p className="App-intro"> Input numbers </p>
-        <p> result is: {this.state.result} </p>
-        <form onSubmit={this.calculate}>
+        <div className="result">
+          <p> result is: {this.state.result} </p>
+        </div>
+        <form className = "inputForm" onSubmit={this.calculate}>
           var1: <div> <input className="inputField" type="number" name="var1" value={this.state.var1} onChange={this.handleChange}></input> </div>
           var2: <div> <input className="inputField" type="number" name="var2" value={this.state.var2} onChange={this.handleChange}></input> </div>
           var3: <div> <input className="inputField" type="number" name="var3" value={this.state.var3} onChange={this.handleChange}></input> </div>
